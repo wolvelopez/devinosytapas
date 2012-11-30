7542dbaa16ea7f554b2cf33a38bddebc08698590
razpeitia <razpeitia@gmail.com>
2012-11-29 22:40:10 -0600

    Serving statics files from MEDIA_ROOT on DEBUG mode

--- devinosytapas/urls.py
+++ devinosytapas/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,3 +19,8 @@ urlpatterns = patterns('',
     url(r'^admin/filebrowser/', include('filebrowser.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^uploads/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    )
